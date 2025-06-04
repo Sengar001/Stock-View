be8e6ae2b3f0bac47b927482fd7739b7d255b5e6
Sengar001 <abhisinghsengar001@gmail.com>
2025-06-04 10:43:22 +0530

    created dto for signup request

---
+++ stockview-backend/src/main/java/abhishek/stockviewbackend/dto/SignupRequest.java
@@ -0,0 +1,18 @@
+package abhishek.stockviewbackend.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest(
+        @NotNull(message = "Email must not be null")
+        @Email(message = "Email should be valid")
+        @JsonProperty("email")
+        String email,
+
+        @NotNull(message = "Password must not be null")
+        @Size(min = 6, max = 40, message = "Password must be between 6 and 40 characters")
+        @JsonProperty("password")
+        String password
+) {}
